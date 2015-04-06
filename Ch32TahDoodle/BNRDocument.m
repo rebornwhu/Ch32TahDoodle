00e8ac1ddf1e3f23e3d48b5eab89c869eea01d39
shawnlu123 <reborntj@gmail.com>
2015-04-05 19:29:00 -0700

    Test comment 2

--- Ch32TahDoodle/BNRDocument.m
+++ Ch32TahDoodle/BNRDocument.m
@@ -18,7 +18,7 @@
     self = [super init];
     if (self) {
         // Add your subclass-specific initialization here.
-        // Test comment
+        // Test comment 2
     }
     return self;
 }
