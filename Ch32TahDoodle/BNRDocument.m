06bfe43f29517e07708a9b550a78e82804c2ce69
rebornwhu <rebornwhu@gmail.com>
2015-04-07 09:44:26 -0700

    Add data saving

--- Ch32TahDoodle/BNRDocument.m
+++ Ch32TahDoodle/BNRDocument.m
@@ -43,16 +43,30 @@
 - (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError {
     // Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
     // You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-    [NSException raise:@"UnimplementedMethod" format:@"%@ is unimplemented", NSStringFromSelector(_cmd)];
-    return nil;
+    if (!self.tasks) {
+        self.tasks = [NSMutableArray array];
+    }
+    
+    NSData *data = [NSPropertyListSerialization
+                    dataWithPropertyList:self.tasks
+                    format:NSPropertyListXMLFormat_v1_0
+                    options:0
+                    error:outError];
+    return data;
+
 }
 
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError {
     // Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
     // You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
     // If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-    [NSException raise:@"UnimplementedMethod" format:@"%@ is unimplemented", NSStringFromSelector(_cmd)];
-    return YES;
+    self.tasks = [NSPropertyListSerialization
+                  propertyListWithData:data
+                  options:NSPropertyListMutableContainers
+                  format:NULL
+                  error:outError];
+    
+    return (self.tasks != nil);
 }
 
 
